9511b6b1ed0457a486cef1da6c6fb5bbf6656e0e
Nicola Bellotti <n.bellotti90@gmail.com>
2020-03-30 11:46:03 +0200

    create card component to handle top sites list

---
+++ components/card/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const CardWrapperStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  padding: 20,
+  boxShadow: '0 0 6px 6px #00000020',
+  marginBottom: 40,
+  color: 'initial'
+};
+
+const CardImageWrapper = {
+  display: 'flex',
+  justifyContnet: 'center',
+  alignItems: 'center'
+};
+
+const ImageWrapper = {
+  height: 150,
+  maxHeight: 150,
+};
+
+const CardContentWrapper = {
+  paddingLeft: 20,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between'
+};
+
+const ContentTitle = {
+  fontSize: 20,
+  fontWeight: 'bold',
+};
+
+export const Card = (props) => {
+  console.log(props);
+  return (<div style={CardWrapperStyle}>
+    <div style={CardImageWrapper}>
+      <img
+        style={ImageWrapper}
+        src={props.image}
+        alt={`${props.title} image`}
+      />
+    </div>
+    <div style={CardContentWrapper}>
+      <div style={ContentTitle}>{props.title}</div>
+      <div>{props.description}</div>
+      <div>{props.url}</div>
+    </div>
+  </div>)
+};
