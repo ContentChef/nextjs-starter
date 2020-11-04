e17935a31926f14c19495cb89fc2a2da00637bef
Alessandro Palumbo <apalumbo@byte-code.com>
2020-11-04 19:33:33 +0100

    Upgraded to next 10, added sample conf and examples for static and dynamic generation

---
+++ pages/top-site-dynamic-blocking/[publicId].js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { TopSite } from '../../components/top-site';
+import {contentChef} from "../../services/contentChefClient";
+
+
+export async function getStaticPaths() {
+  const sites = await contentChef.searchTopSitesContents(['aws-site', 'content-chef-site']);
+  const publicIds = sites.map(site => site.publicId);
+  const paths = publicIds.map(publicId =>{
+    return {params:{ publicId:publicId } }
+  });
+  return {
+    paths: paths,
+    fallback: 'blocking' 
+  };
+}
+
+export async function getStaticProps({ params }) {  
+  const result = await contentChef.getContent(params.publicId);
+
+  if (!result) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    revalidate: 60,
+    props: {
+      renderType: 'Dynamic blocking',
+      topSite: result.payload,
+      publicId: result.publicId,
+      cloudName: result.requestContext.cloudName
+    }
+  }
+}
+
+export default TopSite;
--- pages/top-site/[publicId].js
+++ pages/top-site/[publicId].js
@@ -1,71 +1,23 @@
 import React from 'react';
-import Layout from "../../components/layout";
+import { TopSite } from '../../components/top-site';
 import {contentChef} from "../../services/contentChefClient";
 
-const SiteWrapper = {
-  textAlign: 'center'
-};
-
-const ImageWrapper = {
-  height: 150,
-  maxHeight: 150,
-};
-
-const SiteContentWrapper = {
-  paddingTop: 20,
-  textAlign: 'center'
-};
-
-const SiteTitle = {
-  fontSize: 20,
-  fontWeight: 'bold',
-  marginBottom: '2em'
-};
-
-const SiteDescription = {
-  marginBottom: '1.5em'
-}
-
-const TopSite = ({ topSite, cloudName }) => {
-  return (
-    <Layout
-      title={`${topSite.title} Site`}
-    >
-      {!topSite
-        ? <div>No Content Found</div>
-        : <>
-          <div>
-            <h1 className="title">
-              Welcome to <a href="https://contentchef.io">ContentChef!</a> + <a href="https://nextjs.org">Next.js</a> tutorial
-            </h1>
-            <p className="description">
-              One of the best site
-            </p>
-          </div>
-          <div style={SiteWrapper}>
-            <img
-              className={ImageWrapper}
-              src={contentChef.getImageUrl(topSite.image, cloudName)}
-              alt={`${topSite.title} image`}
-            />
-            <div style={SiteContentWrapper}>
-              <div style={SiteTitle}>{topSite.title}</div>
-              <div style={SiteDescription}>{topSite.description}</div>
-              <div>{topSite.url}</div>
-            </div>
-          </div>
-        </>
-      }
-    </Layout>
-  )
-};
 
 export async function getServerSideProps(context) {
   const { params } = context;
   const result = await contentChef.getContent(params.publicId);
+
+  if (!result) {
+    return {
+      notFound: true
+    }
+  }
+
   return {
     props: {
+      renderType: 'Request time rendered',
       topSite: result.payload,
+      publicId: result.publicId,
       cloudName: result.requestContext.cloudName
     }
   }
